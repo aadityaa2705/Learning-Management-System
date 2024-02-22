0c8ee7d73b580b97f4b0363d11778e55c4d347d1
aditya barathe <adityabarathe100@gmail.com>
2024-02-22 13:03:18 +0530

    changed spring version to 2.6.13

---
+++ src/main/java/com/te/lms/entity/Primary.java
@@ -0,0 +1,37 @@
+package com.te.lms.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.te.lms.entity.enums.BloodGroup;
+import com.te.lms.entity.enums.EmployeeStatus;
+import com.te.lms.entity.enums.Gender;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "primary")
+public class Primary {
+	private String employeeName;
+	private LocalDate dateOfJoining;
+	private LocalDate birthDate;
+	private String designation;
+	private String employeeEmail;
+	private Gender gender;
+	private BloodGroup bloodGroupType;
+	private String nationality;
+	private EmployeeStatus status;
+	
+	
+}
